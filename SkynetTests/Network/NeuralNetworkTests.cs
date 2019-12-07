954811812ad69ffb261170866d6543a5c9e779bc
Acero <dex252@bk.ru>
2019-12-08 03:36:47 +0800

    Relise methods Scaling&Normalization. Change interface for Lear method.

--- SkynetTests/Network/NeuralNetworkTests.cs
+++ SkynetTests/Network/NeuralNetworkTests.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Skynet.Network;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Skynet.Network.Tests
 {
@@ -15,48 +10,48 @@ namespace Skynet.Network.Tests
         [TestMethod()]
         public void FeetForwardTest()
         {
-            var dataset = new List<Tuple<float, float[]>>
+            var outputs = new float[]
             {
-                // Результат - Пациент болен - 1
-                //             Пациент Здоров - 0
-
-                // Неправильная температура T
-                // Хороший возраст A
-                // Курит S
-                // Правильно питается F
-                //                                             T  A  S  F
-                new Tuple<float, float[]>(0, new float[] {0, 0, 0, 0}),
-                new Tuple<float, float[]>(0, new float[] {0, 0, 0, 1}),
-                new Tuple<float, float[]>(1, new float[] {0, 0, 1, 0}),
-                new Tuple<float, float[]>(0, new float[] {0, 0, 1, 1}),
-                new Tuple<float, float[]>(0, new float[] {0, 1, 0, 0}),
-                new Tuple<float, float[]>(0, new float[] {0, 1, 0, 1}),
-                new Tuple<float, float[]>(1, new float[] {0, 1, 1, 0}),
-                new Tuple<float, float[]>(0, new float[] {0, 1, 1, 1}),
-                new Tuple<float, float[]>(1, new float[] {1, 0, 0, 0}),
-                new Tuple<float, float[]>(1, new float[] {1, 0, 0, 1}),
-                new Tuple<float, float[]>(1, new float[] {1, 0, 1, 0}),
-                new Tuple<float, float[]>(1, new float[] {1, 0, 1, 1}),
-                new Tuple<float, float[]>(1, new float[] {1, 1, 0, 0}),
-                new Tuple<float, float[]>(0, new float[] {1, 1, 0, 1}),
-                new Tuple<float, float[]>(1, new float[] {1, 1, 1, 0}),
-                new Tuple<float, float[]>(1, new float[] {1, 1, 1, 1})
+                0,0,1,0,0,0,1,0,1,1,1,1,1,0,1,1
+            };
+            var inputs = new float[,]
+            {
+                {0,0,0,0 },
+                {0,0,0,1 },
+                {0,0,1,0 },
+                {0,0,1,1 },
+                {0,1,0,0 },
+                {0,1,0,1 },
+                {0,1,1,0 },
+                {0,1,1,1 },
+                {1,0,0,0 },
+                {1,0,0,1 },
+                {1,0,1,0 },
+                {1,0,1,1 },
+                {1,1,0,0 },
+                {1,1,0,1 },
+                {1,1,1,0 },
+                {1,1,1,1 },
             };
 
             var topology = new Topology(0.01f, 4, 1, 2);
             var neuralNetwork = new NeuralNetwork(topology);
-            var difference = neuralNetwork.Learn(dataset, 400000);
+            var difference = neuralNetwork.Learn(outputs, inputs, 100000);
 
             var results = new List<float>();
-            foreach (var data in dataset)
+      
+
+            for (int i = 0; i < outputs.Length; i++)
             {
-                results.Add(neuralNetwork.FeetForward(data.Item2).Output);
+                var row = neuralNetwork.GetRow(inputs, i);
+                var res = neuralNetwork.FeetForward(row).Output;
+                results.Add(res);
             }
 
             for (int i = 0; i < results.Count; i++)
             {
-                var excepted = Math.Round(dataset[i].Item1, 3);
-                var actual = Math.Round(results[i], 3);
+                var excepted = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
                 Assert.AreEqual(excepted, actual);
             }
 
